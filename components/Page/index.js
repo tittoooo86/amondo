94885f560ad0de645597b28d9b52791e2e8499aa
Titusz Magyar <tittoooo86@gmail.com>
2018-11-24 12:40:08 +0100

    Project setup

---
+++ components/Page/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import Meta from "../Meta";
+
+const theme = {
+    black: "#000000",
+    yellow: "#ffe927",
+    text: "#b3b3b3",
+    white: "#ffffff",
+    maxWidth: "1040px",
+    bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)",
+    gradient: "linear-gradient(-180deg, transparent, rgba(0, 0, 0, 0.9) 97%);"
+};
+const StyledPage = styled.div`
+    background: ${props => props.theme.black};
+    color: ${props => props.theme.text};
+`;
+
+const Inner = styled.div`
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+    padding: 0 20px;
+`;
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'franklin-gothic-urw-comp', sans-serif;;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'franklin-gothic-urw-comp', sans-serif; }
+`;
+export default class Page extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Inner>{this.props.children}</Inner>
+                </StyledPage>
+            </ThemeProvider>
+        );
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Home = props => (
+    <div>
+        <img src="/static/amondo-white.svg" alt="Amondo logo" />
+    </div>
+);
+
+export default Home;
